c93ddc851d216437d8409d404cacf7cdfb6190b9
Karan Janthe <karanjanthe@gmail.com>
2024-06-27 15:28:49 +0530

    fixed theme
    Signed-off-by: Karan Janthe <karanjanthe@gmail.com>

--- apps/pwa/src/App.tsx
+++ apps/pwa/src/App.tsx
@@ -7,21 +7,29 @@ import { AuthProvider } from "./contexts/authContext";
 import { Suspense } from "react";
 import { Spin } from "antd";
 import ErrorBoundary from "antd/es/alert/ErrorBoundary";
+import { HelmetProvider } from "react-helmet-async";
+import { App as AntdApp } from "antd";
 
 export const App = () => {
   return (
-    <AntdThemeProvider>
-      <trpc.Provider client={trpcClient} queryClient={queryClient}>
-        <QueryClientProvider client={queryClient}>
-          <ErrorBoundary>
-            <Suspense fallback={<Spin size="large" />}>
-              <AuthProvider>
-                <RouterProvider router={router} />
-              </AuthProvider>
-            </Suspense>
-          </ErrorBoundary>
-        </QueryClientProvider>
-      </trpc.Provider>
-    </AntdThemeProvider>
+    <HelmetProvider>
+      <AntdThemeProvider>
+        <trpc.Provider client={trpcClient} queryClient={queryClient}>
+          <QueryClientProvider client={queryClient}>
+            <ErrorBoundary>
+              <Suspense
+                fallback={<Spin size="large" style={{ height: "50%" }} />}
+              >
+                <AuthProvider>
+                  <AntdApp>
+                    <RouterProvider router={router} />
+                  </AntdApp>
+                </AuthProvider>
+              </Suspense>
+            </ErrorBoundary>
+          </QueryClientProvider>
+        </trpc.Provider>
+      </AntdThemeProvider>
+    </HelmetProvider>
   );
 };
