c30897f3d365b8e9e63a130c6bb6c098fe2a700b
Karan Janthe <karanjanthe@gmail.com>
2024-08-29 13:24:44 +0530

    better event tracking and added common config again
    Signed-off-by: Karan Janthe <karanjanthe@gmail.com>

--- apps/pwa/src/App.tsx
+++ apps/pwa/src/App.tsx
@@ -5,20 +5,40 @@ import GlobalDrawer from "./components/globalDrawer/GlobalDrawer";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
 import { MotionLazy } from "./components/animate/motion-lazy";
+import { clarity } from 'react-microsoft-clarity';
+import { useEffect } from 'react';
+import { PostHogProvider } from 'posthog-js/react'
+import { APP_FAVICON, CLARITY_ID, META_DESCRIPTION, META_TITLE } from "./appConfig";
+
+// Add these constants for PostHog configuration
+const POSTHOG_KEY = import.meta.env.VITE_POSTHOG_KEY;
+const POSTHOG_HOST = import.meta.env.VITE_POSTHOG_HOST || 'https://us.i.posthog.com';
 
 export const App = () => {
+  useEffect(() => {
+    clarity.init(CLARITY_ID);
+  }, []);
+
   return (
-    <AntdConfig>
-      <AntdApp>
-        <MotionLazy>
-          <Helmet>
-            <title>Tezi React-fastify boilerplate</title>
-            {/* <link rel="icon" href={"path to your favicon"} /> */}
-          </Helmet>
-          <GlobalDrawer />
-          <RouterProvider router={router} />
-        </MotionLazy>
-      </AntdApp>
-    </AntdConfig>
+    <PostHogProvider 
+      apiKey={POSTHOG_KEY}
+      options={{
+        api_host: POSTHOG_HOST,
+      }}
+    >
+      <AntdConfig>
+        <AntdApp>
+          <MotionLazy>
+            <Helmet>
+            <title>{META_TITLE}</title>
+            <meta name="description" content={META_DESCRIPTION} />
+            <link rel="icon" href={APP_FAVICON} />
+            </Helmet>
+            <GlobalDrawer />
+            <RouterProvider router={router} />
+          </MotionLazy>
+        </AntdApp>
+      </AntdConfig>
+    </PostHogProvider>
   );
 };
